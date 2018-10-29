cb7ea83f60b5cf5980e9ea036707e771881120d0
Паша <@DESKTOP-3C2NDOK>
2018-10-29 13:28:07 +0300

    Firs Commit.

--- NetworkMonitor/App.xaml.cs
+++ NetworkMonitor/App.xaml.cs
@@ -1,17 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using NetworkMonitor.ViewModel;
 using System.Windows;
 
 namespace NetworkMonitor
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainViewModel mainViewModel = new MainViewModel();
+            View.MainWindow mainWindow = new View.MainWindow(mainViewModel);
+            mainWindow.Show();
+        }
     }
 }
---
+++ NetworkMonitor/View/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using NetworkMonitor.ViewModel;
+using System.Windows;
+
+namespace NetworkMonitor.View
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow(MainViewModel mainViewModel)
+        {
+            InitializeComponent();
+            DataContext = mainViewModel;
+        }
+    }
+}
